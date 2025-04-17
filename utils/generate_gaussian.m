2a2bdacd9cadde12de24f6596865df42917feb87
vejaxu <wjxu29@gmail.com>
2025-04-17 22:24:35 +0800

    Update generate_gaussian.m

--- utils/generate_gaussian.m
+++ utils/generate_gaussian.m
@@ -1,6 +1,6 @@
 % 生成数据
-n = 5000;
-w = 2000;
+n = 2000;
+w = 5;
 data0 = zeros(n,w);
 data1 = randn(n,w);
 data2 = randn(n,w);
@@ -9,7 +9,7 @@ class = ones(2*n,1);
 class(n+1:end) = 2;
 
 % 生成文件名，包含 w 的值
-filename = sprintf('Data/w%dGaussians.mat', w);
+filename = sprintf('../../Data/w%dGaussians.mat', w);
 
 % 保存数据
 save(filename, 'data', 'class');
